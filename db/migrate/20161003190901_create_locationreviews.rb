f33d1ff481b9f6dbc0f6e7f688b01d97bc91278c
ebdemoll <ebdem4@gmail.com>
2016-10-03 15:10:41 -0400

    Created locationreviews migration

---
+++ db/migrate/20161003190901_create_locationreviews.rb
@@ -0,0 +1,10 @@
+class CreateLocationreviews < ActiveRecord::Migration[5.0]
+  def change
+    create_table :locationreviews do |t|
+      t.belongs_to :location, null: false
+      t.belongs_to :review, null: false
+
+      t.timestamps 
+    end
+  end
+end
