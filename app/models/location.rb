ee3c40f44bab3d3c50aaca432ab6bdc13224bea8
ebdemoll <ebdem4@gmail.com>
2016-10-03 15:24:30 -0400

    created location model

---
+++ app/models/location.rb
@@ -0,0 +1,10 @@
+class Location < ActiveRecord::Base
+  has_many :locationreviews
+  has_many :reviews, through: :locationreviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true, numericality: { only_integer: true }, length: { is: 5 }
+end
