f1c1d79c2de5a52dcd40b39c28abd90b30dbf0f8
ebdemoll <ebdem4@gmail.com>
2016-10-03 15:13:47 -0400

    created comments migration

---
+++ db/migrate/20161003191134_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.text :body, null: false
+      t.belongs_to :review, null: false
+      t.belongs_to :user, null:false
+
+      t.timestamps 
+    end
+  end
+end
