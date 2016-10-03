6d459c0aaae19e4bbe6aef80ec3da7ef0cce17cd
ebdemoll <ebdem4@gmail.com>
2016-10-03 15:08:18 -0400

    Created reviews migration

---
+++ db/migrate/20161003190600_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reviews do |t|
+      t.belongs_to :user, null: false
+      t.integer :rating, null: false
+      t.text :body
+      t.string :flavor, null: false 
+
+      t.timestamps
+    end
+  end
+end
