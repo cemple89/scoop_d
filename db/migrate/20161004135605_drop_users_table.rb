eda1afd9bdc38f410033aa191c95940d09be99e9
Nathan W <nate01776@gmail.com>
2016-10-04 09:57:06 -0400

    added drop users table migration

---
+++ db/migrate/20161004135605_drop_users_table.rb
@@ -0,0 +1,4 @@
+class DropUsersTable < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003191134) do
+ActiveRecord::Schema.define(version: 20161003195220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,12 @@ ActiveRecord::Schema.define(version: 20161003191134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
