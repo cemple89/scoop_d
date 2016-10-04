d29773c60a81b4e770de78261e2a4271db709000
Nathan W <nate01776@gmail.com>
2016-10-04 09:47:55 -0400

    added first basic login tests

--- spec/features/01_unauth_user_can_sign_in_spec.rb
+++ spec/features/01_unauth_user_can_sign_in_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
 
-feature "Unauthorized signs in" do
+feature "Unauthorized can login" do
   let(:user) do
     User.create(
       username: "test_user"
     )
   end
 
-  scenario "" do
+  scenario "User sees login button in header" do
+    visit '/'
+    expect(page).to have_link ("Login")
+  end
+
+  scenario "User can click login button and login" do
+    visit '/'
+    click_link ("Login")
+
+    sign_in_as username
+    expect(page).to have_content("test_user successfully signed in!")
+  end
+
+  scenario "User information displayed at top of index and location pages" do
+
+  end
+
+  scenario "Logged in User can log out from button in header" do
 
   end
 end
