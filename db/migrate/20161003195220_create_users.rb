91aa546242a67c96dff20bbf27e11601232001b2
Nathan W <nate01776@gmail.com>
2016-10-04 09:50:16 -0400

    before user table migration

--- db/migrate/20161003195220_create_users.rb
+++ db/migrate/20161003195220_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
+      t.string :username, null: false, uniqueness: true
 
       t.timestamps
     end
