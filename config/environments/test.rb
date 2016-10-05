ace4c493eca227e499defdcb6b2a31a9f734987a
Sophie Heller <sophieheller1@gmail.com>
2016-10-05 16:13:28 -0400

    Hound commits

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # frozen_string_literal: true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: "http://localhost:3000",
+    user_name: "scoopdmailer",
+    password: "scoopdrocks",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
