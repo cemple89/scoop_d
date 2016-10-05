825549e7a39ff22dcdc7321c09e7c291549ddecc
Sophie Heller <sophieheller1@gmail.com>
2016-10-05 16:48:48 -0400

    Reordered tests

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161005133307) do
-=======
 ActiveRecord::Schema.define(version: 20161005174456) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
