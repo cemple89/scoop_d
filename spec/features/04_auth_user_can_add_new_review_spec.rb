2b41f13ae6024bc48e31c798d1eac749ebd853f8
Charlie Emple <charlie.emple@gmail.com>
2016-10-05 14:06:46 -0400

    formatted show page for locations & updated review tests

--- spec/features/04_auth_user_can_add_new_review_spec.rb
+++ spec/features/04_auth_user_can_add_new_review_spec.rb
@@ -28,21 +28,25 @@ feature "users can add reviews for locations" do
     expect(page).to have_content "Review added successfully"
     expect(page).to have_content forge_ice_cream_bar.name
     expect(page).to have_content 5
-    expect(page).to have_content forge_ice_cream_bar.flavor
+    expect(page).to have_content "Rocky Road"
     expect(page).to have_content "This is a raving review!"
   end
 
   scenario "adds a review for a location unsuccessfully" do
+    visit "/"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button ("Log In")
     forge_ice_cream_bar = Location.create(name: 'Forge Ice Cream Bar', address: '626 Somerville Ave.', city: 'Somerville', state: 'MA', zip_code: '02143')
 
     visit location_path(forge_ice_cream_bar)
 
     click_link "Add a Review"
     expect(page).to have_content "Review Form for Forge Ice Cream Bar"
-
+    choose("five scoops")
 
     click_button "Add Review"
 
-    expect(page).to have_content "Body can't be blank"
+    expect(page).to have_content "Flavor can't be blank"
   end
 end
