a04223c6ed7664a8a99846c2d476a94e3791116b
Sophie Heller <sophieheller1@gmail.com>
2016-10-05 10:30:56 -0400

    Added tests for admin designation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,9 @@
 class LocationsController < ApplicationController
   def index
-  end 
+    if current_user
+      if current_user.email == "sophieheller1@gmail.com" || current_user.email == "ebdem4@gmail.com" || current_user.email == "nate01776@gmail.com" || current_user.email == "charlie.emple@gmail.com"
+        current_user.update_attribute :admin, true
+      end
+    end
+  end
 end
