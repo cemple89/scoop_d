caddef4fe2585d1e02bc9d5ae0553c1c4c948a29
Charlie Emple <charlie.emple@gmail.com>
2016-10-05 14:17:11 -0400

    single quotes fix

--- spec/features/05_auth_user_sees_list_of_all_locations_spec.rb
+++ spec/features/05_auth_user_sees_list_of_all_locations_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
-feature "users sees a list of locations" do
+feature 'users sees a list of locations' do
   let(:user) do
     User.create(
-      email: "test_user@gmail.com",
-      password: "scooped"
+      email: 'test_user@gmail.com',
+      password: 'scooped'
     )
   end
-  scenario "sees a list of locations and link for new location" do
+  scenario 'sees a list of locations and link for new location' do
     forge_ice_cream_bar = Location.create(name: 'Forge Ice Cream Bar', address: '626 Somerville Ave.', city: 'Somerville', state: 'MA', zip_code: '02143')
-    toscaninis = Location.create(name: "Toscanini's", address: "25 Main Street", city: "Cambridge", state: "MA", zip_code: "02139", neighborhood: "Central")
+    toscaninis = Location.create(name: 'Toscanini\'s', address: '25 Main Street', city: 'Cambridge', state: 'MA', zip_code: '02139', neighborhood: 'Central')
 
-    visit "/"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button ("Log In")
+    visit '/'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button ('Log In')
     visit locations_path
 
-    expect(page).to have_content "Forge Ice Cream Bar"
-    expect(page).to have_link "Toscanini's"
+    expect(page).to have_content 'Forge Ice Cream Bar'
+    expect(page).to have_link 'Toscanini\'s'
 
-    click_link "Add New Location"
+    click_link 'Add New Location'
 
-    expect(page).to have_content "New Location Form"
+    expect(page).to have_content 'New Location Form'
   end
-  scenario "clicks link and is taken to show page for given restaurant" do
-    toscaninis = Location.create(name: "Toscanini's", address: "25 Main Street", city: "Cambridge", state: "MA", zip_code: "02139", neighborhood: "Central")
+  scenario 'clicks link and is taken to show page for given restaurant' do
+    toscaninis = Location.create(name: 'Toscanini\'s', address: '25 Main Street', city: 'Cambridge', state: 'MA', zip_code: '02139', neighborhood: 'Central')
 
-    visit "/"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button ("Log In")
+    visit '/'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button ('Log In')
     visit root_path
 
-    click_link "Toscanini's"
+    click_link 'Toscanini\'s'
 
     expect(page).to have_content toscaninis.name
     expect(page).to have_content toscaninis.address
