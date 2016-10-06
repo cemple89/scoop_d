52b8eb31eff00eefe6772df370550cf4c338ba36
Charlie Emple <charlie.emple@gmail.com>
2016-10-06 11:35:13 -0400

    votes model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews
+  has_many :votes 
 
   validates :email, presence: true
 end
---
+++ app/models/vote.rb
@@ -0,0 +1,7 @@
+class Vote < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :review
+
+  validates :user, presence: true
+  validates :review, presence: true
+end
