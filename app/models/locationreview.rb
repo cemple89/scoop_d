babc22a6ea18924da0736495db19a9abb5a2ecbb
Sophie Heller <sophieheller1@gmail.com>
2016-10-06 10:07:05 -0400

    Removed unnecessary files

--- app/models/locationreview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class Locationreview < ActiveRecord::Base
-  belongs_to :location
-  belongs_to :review
-
-  validates :location, presence: true
-  validates :review, presence: true
-end
