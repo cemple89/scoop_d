cbd113e80e560accad0c4bd95973915bbee5e472
Nathan W <nate01776@gmail.com>
2016-10-07 11:04:38 -0400

    carrier wave sending to was

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'listen'
 gem 'actionmailer', '~> 5.0', '>= 5.0.0.1'
 gem 'rubocop'
 gem 'carrierwave', '>= 1.0.0.beta', '< 2.0'
+gem 'figaro'
+gem 'fog'
 group :development, :test do
   gem 'capybara'
   gem 'factory_girl_rails'
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,1 +1,14 @@
 require 'carrierwave/orm/activerecord'
+
+
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["aws_access_id"],
+      aws_secret_access_key: ENV["aws_access_secret_key"],
+      region: 'us-west-2'
+  }
+  config.fog_directory  = "scoopduserimages"
+  config.fog_public     = false
+end
