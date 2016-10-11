4c328594a6fdcc5d8bf28a76d0e2ea8093b7db21
Nathan W <nate01776@gmail.com>
2016-10-11 17:21:02 -0400

    image test rewritten - no longer commits to AWS

--- spec/features/11_user_can_add_profile_image_spec.rb
+++ spec/features/11_user_can_add_profile_image_spec.rb
@@ -9,30 +9,39 @@ describe ImageUploader do
   let(:user) { User.create(email: 'testemail@gmail.com', password: 'alsoscooped' ) }
   let(:user2) { User.create(email: 'testemail2@gmail.com', password: 'scooped') }
   let(:uploader) { ImageUploader.new(user, :image) }
+  feature "User can upload profile image" do
+    scenario 'User can see upload link if not added file' do
+      visit '/'
+      fill_in 'Email', with: user2.email
+      fill_in 'Password', with: user2.password
+      click_button 'Log In'
 
-  it 'User can see upload link if not added file' do
-    visit '/'
-    fill_in 'Email', with: user2.email
-    fill_in 'Password', with: user2.password
-    click_button 'Log In'
+      expect(page).to have_link('Add Image')
+    end
 
-    expect(page).to have_link('Add Image')
+    scenario 'User can see upload link if not added file' do
+      visit '/'
+      fill_in 'Email', with: user2.email
+      fill_in 'Password', with: user2.password
+      click_button 'Log In'
+
+      expect(page).to have_link('Add Image')
+    end
+
+    scenario 'User can upload file' do
+      visit '/'
+      fill_in 'Email', with: user2.email
+      fill_in 'Password', with: user2.password
+      click_button 'Log In'
+
+      click_link('Add Image')
+      attach_file('image-file', './spec/features/images/doge.jpg')
+
+      click_button('Update User')
+
+      expect(page).to_not have_link('Add Image')
+
+      expect(page).to have_css('img[src*=doge]')
+    end
   end
-
-  it 'User can upload file' do
-    visit '/'
-    fill_in 'Email', with: user2.email
-    fill_in 'Password', with: user2.password
-    click_button 'Log In'
-
-    click_link('Add Image')
-    attach_file('image-file', './spec/features/images/doge.jpg')
-
-    click_button('Update User')
-
-    expect(page).to_not have_link('Add Image')
-
-    page.find('#user-image')['src'].should have_content 'doge.jpg'
-  end
-
 end
