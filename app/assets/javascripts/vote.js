fa11e6bc3f4acae3b09ed06942446279636e89f5
Charlie Emple <charlie.emple@gmail.com>
2016-10-11 15:16:47 -0400

    conditional to delete/recreate votes

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -21,26 +21,23 @@ $(document).ready(function() {
   });
 
 
-  // $('.js-downvote').on('click', function(event){
-  //   event.preventDefault();
-  //   var self = $(this),
-  //       review = self.data('review'),
-  //       count = self.data('value'),
-  //       user = self.data('user');
-  //
-  //   console.log(review)
-  //   console.log(user)
-  //   console.log(count)
-  //   console.log("test data")
-  //
-  //   var down_request = $.ajax({
-  //     method: "POST",
-  //     URL: '/location/review$(id)/downvote'
-  //   });
-  //
-  //   down_request.done(function(data) {
-  //     // what goes here?!?!?!?
-  //   })
-  // });
+  $('.js-downvote').on('click', function(event){
+    event.preventDefault();
+    var self = $(this),
+        location = self.data('location'),
+        review = self.data('review'),
+        count = self.data('value'),
+        user = self.data('user');
+
+    var down_request = $.ajax({
+      method: "POST",
+      data: {review_id: review, location_id: location, count: count},
+      URL: '/locations/`$(location)`/reviews/`$(review)`/vote'
+    });
+
+    down_request.done(function(data) {
+      self.css('color', 'red')
+    })
+  });
 
 });
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,20 +6,50 @@ class VotesController < ApplicationController
     review = Review.find(params[:review_id])
     value = params[:count]
     vote = Vote.find_by(review: review.id, user: user.id)
-
     if vote.present?
-      vote.destroy
+      if vote.count != value.to_i
+        vote.destroy
+        @vote = Vote.create(
+          user: user,
+          review: review,
+          count: value
+        )
+      elsif vote.count == value.to_i
+        vote.destroy
+      end
+    else vote.nil?
+      @vote = Vote.create(
+        user: user,
+        review: review,
+        count: value
+      )
     end
 
-    @vote = Vote.create(
-      user: user,
-      review: review,
-      count: value
-    )
 
-    respond_to do |format|
-      format.json {render :json => @vote }
+    #
+    # userdownvote = Vote.find_by(count: -1,review_id: @review.id, user_id: current_user.id)
+    # userupvote = Vote.find_by(count: 1,review_id: @review.id, user_id: current_user.id)
+    # if userdownvote.nil?
+    #   if userupvote.nil?
+    #     vote = @review.votes.create
+    #     vote.update_attribute(:user_id, current_user.id)
+    #     vote.update_attribute(:count, -1)
+    #     redirect_to location_path(@review.location)
+    #   else
+    #     userupvote.delete
+    #     vote = @review.votes.create
+    #     vote.update_attribute(:user_id, current_user.id)
+    #     vote.update_attribute(:count, -1)
+    #     redirect_to location_path(@review.location)
+    #   end
+    # else userupvote.nil?
+    #   userdownvote.delete
+    #   redirect_to location_path(@review.location)
+    # end
+    #
+      respond_to do |format|
+        format.json {render :json => @vote }
+      end
     end
-  end
 
 end
