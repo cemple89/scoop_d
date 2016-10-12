52eef6d3328d72be54ce60696a81fc17e2da3866
Charlie Emple <charlie.emple@gmail.com>
2016-10-12 12:10:41 -0400

    added JSON files for all locations, alphabetized neighborhoods

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161007135826) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +62,6 @@ ActiveRecord::Schema.define(version: 20161007135826) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "avatar"
     t.boolean  "admin",                  default: false
     t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
