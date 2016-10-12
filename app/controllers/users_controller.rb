2f1baf7477249f1e3e2a8fc229e8bd4274b5cdfc
Sophie Heller <sophieheller1@gmail.com>
2016-10-12 13:14:32 -0400

    Amended destroy method to change user_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,12 @@ class UsersController < ApplicationController
   def destroy
     if current_user.admin?
       @user = User.find(params[:id])
+      @locations = Location.find_by(user_id: @user)
+      @locations.update_attribute(:user_id, -1)
+      @reviews = Review.find_by(user_id: @user)
+      @reviews.update_attribute(:user_id, -1)
+      @votes = Vote.find_by(user_id: @user)
+      @votes.destroy
       @user.destroy
       flash[:notice] = 'User was successfully deleted'
       redirect_to users_path
