e20fbce3a8e1807959ce06b16780aac45cc2e263
Charlie Emple <charlie.emple@gmail.com>
2016-10-12 14:35:13 -0400

    locations seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,34 +11,34 @@ require 'json'
 
 # https://www.yelp.com/developers/api_console
 
-Dir.foreach("./db/JSONdata") do |file|
-  next if file == "." or file == ".."
-
-  location_data = File.read("./db/JSONdata/" + file)
-  sorted_location_data = JSON.parse(location_data)
-
-  sorted_location_data.each do |key, value|
-    if key == 'businesses'
-      value.each do |b_key|
-        if Location.find_by(phone_number: b_key["display_phone"]) != nil
-          next
-        else
-          Location.create(
-            name: b_key['name'],
-            address: b_key["location"]['address'],
-            city: b_key["location"]['city'],
-            state: b_key["location"]['state_code'],
-            zip_code: b_key["location"]['postal_code'],
-            neighborhood: b_key["location"]['neighborhoods'],
-            phone_number: b_key["display_phone"],
-            image_url: b_key["image_url"],
-            user_id: -1
-          )
-        end
-      end
-    end
-  end
-end
+# Dir.foreach("./db/JSONdata") do |file|
+#   next if file == "." or file == ".."
+#
+#   location_data = File.read("./db/JSONdata/" + file)
+#   sorted_location_data = JSON.parse(location_data)
+#
+#   sorted_location_data.each do |key, value|
+#     if key == 'businesses'
+#       value.each do |b_key|
+#         if Location.find_by(phone_number: b_key["display_phone"]) != nil
+#           next
+#         else
+#           Location.create(
+#             name: b_key['name'],
+#             address: b_key["location"]['address'],
+#             city: b_key["location"]['city'],
+#             state: b_key["location"]['state_code'],
+#             zip_code: b_key["location"]['postal_code'],
+#             neighborhood: b_key["location"]['neighborhoods'],
+#             phone_number: b_key["display_phone"],
+#             image_url: b_key["image_url"],
+#             user_id: -1
+#           )
+#         end
+#       end
+#     end
+#   end
+# end
 
 
 
