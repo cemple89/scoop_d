e6d8802a97f38c53167d9787d4c4de1d4e2dbf35
Sophie Heller <sophieheller1@gmail.com>
2016-10-12 10:54:33 -0400

    All tests passing for search

---
+++ spec/features/12_user_searches_for_location_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'factory_girl_rails'
+
+feature 'Authenticated user searches for location' do
+  scenario 'searches for location that exists' do
+    user = FactoryGirl.create(:user)
+    location = FactoryGirl.create(:location)
+    visit '/'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button('Log In')
+
+    fill_in 'search', with: location.name
+    click_button 'Search'
+    expect(page).to have_content('Search Results')
+    expect(page).to have_content(location.name)
+  end
+
+  scenario 'searches for location that does not exist' do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button('Log In')
+
+    fill_in 'search', with: 'Friendlys'
+    click_button 'Search'
+    expect(page).to have_content('Search Results')
+    expect(page).to have_content('Sorry, your search yielded no locations. Please add the location below!')
+
+  end
+end
