ba31d8db0b01b5f8098b022f35b2b15b8640e82a
Charlie Emple <charlie.emple@gmail.com>
2016-10-13 15:21:12 -0400

    hound fixes

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -21,15 +21,15 @@ $(document).ready(function() {
 
     up_request.done(function(data) {
       if (data === null) {
-        vote_total.html(vote_total_num -= 1)
+        vote_total.html(vote_total_num -= 1);
         var x =$("<i class='fa fa-times removed'></i>");
         $(vote_sprinkles).append(x);
         x.slideDown(250).delay(2000).slideUp(250);
       } else {
-        vote_total.html(vote_total_num += 1)
+        vote_total.html(vote_total_num += 1);
         var x =$("<i class='fa fa-cloud-upload added'></i>");
         $(vote_sprinkles).append(x);
-        x.slideDown(250).delay(2000).slideUp(250)
+        x.slideDown(250).delay(2000).slideUp(250);
       }
     })
   });
@@ -43,7 +43,7 @@ $(document).ready(function() {
         count = self.data('value'),
         user = self.data('user');
         var vote_total = $("#review-count-" + review);
-        var vote_total_num = parseInt(vote_total.html(), 10)
+        var vote_total_num = parseInt(vote_total.html(), 10);
     var vote_sprinkles = $("#sprinkles-" + review);
 
     var down_request = $.ajax({
@@ -54,15 +54,15 @@ $(document).ready(function() {
 
     down_request.done(function(data) {
       if (data === null) {
-        vote_total.html(vote_total_num += 1)
-        var x =$("<i class='fa fa-times removed'></i>");
-        $(vote_sprinkles).append(x);
-        x.slideDown(250).delay(2000).slideUp(250);
+        vote_total.html(vote_total_num += 1);
+        var y =$("<i class='fa fa-times removed'></i>");
+        $(vote_sprinkles).append(y);
+        y.slideDown(250).delay(2000).slideUp(250);
       } else {
-        vote_total.html(vote_total_num -= 1)
-        var x =$("<i class='fa fa-cloud-download downvoted'></i>");
-        $(vote_sprinkles).append(x);
-        x.slideDown(250).delay(2000).slideUp(250)
+        vote_total.html(vote_total_num -= 1);
+        var y =$("<i class='fa fa-cloud-download downvoted'></i>");
+        $(vote_sprinkles).append(y);
+        y.slideDown(250).delay(2000).slideUp(250);
       }
     })
   });
